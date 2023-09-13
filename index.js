db6cb34ad1156b9ff83282b91e5231a6ab25b488
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-09-13 18:45:53 -0500

    Add handlers for number and clear buttons

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 let firstOperand, secondOperand, operation;
+let display = "0";
+
+init();
+
+function init() {
+  Array.from(document.getElementsByClassName("digit")).forEach((digitBtn) =>
+    digitBtn.addEventListener("click", handleDigitClick)
+  );
+
+  document.getElementById("clear").addEventListener("click", handleClearClick);
+}
+
+function handleDigitClick(e) {
+  let digit = e.target.innerText;
+  // string concat
+  updateDisplay(display === "0" ? digit : display + digit);
+}
+
+function handleClearClick(e) {
+  updateDisplay(0);
+}
+
+function updateDisplay(value) {
+  display = value.toString();
+  document.getElementById("display").innerText = display;
+}
 
 function operate(operation, a, b) {
   return operation(a, b);
